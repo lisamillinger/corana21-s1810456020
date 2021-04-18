8b3edcb72726e3f23923c554a9bdaf092aa5ea4d
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-04-18 11:00:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# corana21-s1810456020
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/corana21-s1810456020)
---
+++ src/app/shared/location.ts
@@ -0,0 +1,3 @@
+export class Location {
+  constructor(public id:number, public postcode:number, public city:string, public address: string, public description: string, public title: string) {}
+}
