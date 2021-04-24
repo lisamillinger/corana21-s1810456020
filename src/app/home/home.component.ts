84569ccf80f04f2973e15c8c0f1ae887c01fcd0d
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-04-24 18:21:05 +0200

    5 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "bs-home",
+  template: `
+    <div class="ui container">
+      <h1>Happy Impfen :)</h1>
+      <a routerLink="../locations" class="ui red button">
+        Impfungen
+      </a>
+    </div>
+  `,
+  styles: []
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HomeComponent {}
