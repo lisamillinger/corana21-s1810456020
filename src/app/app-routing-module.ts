516f1f1fb0341b9e009296a42e5064d21dc3eef3
lisamillinger <s1810456020@students.fh-hagenberg.at>
2021-04-25 16:56:47 +0200

    2 files modified

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -4,15 +4,13 @@ import { LocationDetailsComponent } from
 './location-details/location-details.component';
 import { LocationListComponent } from './location-list/location-list.component';
 import { HomeComponent } from './home/home.component';
-import { VaccinationListComponent } from './vaccination-list/vaccination-list.component';
 
 
 const routes: Routes = [
 { path: '', redirectTo: 'home', pathMatch: 'full' },
 { path: 'home', component: HomeComponent },
 { path: 'locations', component: LocationListComponent },
-{ path: 'locations/:title', component: LocationDetailsComponent },
-{ path: 'vaccinations', component: VaccinationListComponent}
+{ path: 'locations/:title', component: LocationDetailsComponent }
 ];
 @NgModule({
 imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,10 @@ import { allLocationsService } from './shared/all-locations.service';
 import { AppRoutingModule } from './app-routing-module';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { VaccinationListComponent } from './vaccination-list/vaccination-list.component';
-import { VaccinationDetailsComponent } from './vaccination-details/vaccination-details.component';
-import { VaccinationListItemComponent } from './vaccination-list-item/vaccination-list-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, LocationListComponent, LocationListItemComponent, LocationDetailsComponent, HomeComponent, VaccinationListComponent, VaccinationDetailsComponent, VaccinationDetailsComponent, VaccinationListItemComponent],
+  declarations: [ AppComponent, HelloComponent, LocationListComponent, LocationListItemComponent, LocationDetailsComponent, HomeComponent],
   bootstrap:    [ AppComponent ],
   providers: [allLocationsService]
 })
